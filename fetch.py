10aa2cfe4309a97f69018601edc827437b835e23
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-02-17 21:09:38 +0100

    Writing rar bands to file

--- fetch.py
+++ fetch.py
@@ -2,6 +2,11 @@
 
 from rar import fetch_rar_lineup
 
+def write_bands(bands, out):
+  with open(out, 'w') as out_file:
+    for band in bands:
+      out_file.write("{}\n".format(band))
+
 if __name__ == "__main__":
-  bands = fetch_rar_lineup()
-  print(bands)
+  rar_bands = fetch_rar_lineup()
+  write_bands(rar_bands, "rar_bands")
