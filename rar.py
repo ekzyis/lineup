12452975e39e706916d3b51a42935e7e246384b2
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-02-17 21:05:16 +0100

    refactor

---
+++ rar.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_rar_lineup():
+  lineup_url = "https://www.rock-am-ring.com/lineup"
+  r = requests.get(lineup_url)
+  lineup_page = BeautifulSoup(r.text, features="html.parser")
+  band_tags = lineup_page.select("div.Appearance > a > h5 > span")
+  bands = [tag.string for tag in band_tags]
+  return bands
+
