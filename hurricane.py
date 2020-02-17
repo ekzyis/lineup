2554ec9f3c24d2954e7f0eb2853d85a32e4f8908
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-02-17 22:29:59 +0100

    Now fetching all hurricane bands

--- hurricane.py
+++ hurricane.py
@@ -4,20 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+def get_bands_from_page(page):
+  soup = BeautifulSoup(page, features="html.parser")
+  band_tags = soup.select("a[routerLinkActive]")
+  bands = [tag["href"].replace("#/artist/", "").replace("-", " ") for tag in band_tags]
+  return bands
 
 def fetch_hurricane_lineup():
-  lineup_url = "https://www.hurricane.de/en/line-up"
+  lineup_url = "https://www.hurricane.de/en/line-up#/artists/alphabetical"
   browser = webdriver.Firefox()
   browser.get(lineup_url)
-  bands_selector = "a[routerLinkActive]"
-  try:
-    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, bands_selector)))
-  except TimeoutException:
-    print("Lineup elements were not found!")
-    browser.close()
-    return []
-  lineup_page = BeautifulSoup(browser.page_source, features="html.parser")
-  band_tags = lineup_page.select("a[routerLinkActive]")
-  bands = [tag["href"].replace("#/artist/", "").replace("-", " ") for tag in band_tags]
+  # scroll down until no new bands are lazily loaded
+  old_bands = []
+  while True:
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    sleep(5)
+    bands = get_bands_from_page(browser.page_source)
+    if len(old_bands) == len(bands):
+      break
+    old_bands = bands
   browser.close()
   return bands
